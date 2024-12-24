490516389d0bd442b30e9b9292c330fe712a6cc5
ms32-org <ms32-org@outlook.com>
2024-12-24 12:26:31 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,25 @@
 const express = require('express');
 const http = require('http');
-const cors = require('cors')
+const cors = require('cors');
 const WebSocket = require('ws');
 const app = express();
 
-app.use(cors())
+app.use(cors());
+app.use(express.raw({ type: 'application/octet-stream' }));  
+
 let screenshotData = null;
+
+app.post('/screenshot', (req, res) => {
+    screenshotData = req.body;
+    console.log("Screenshot data received and stored.");
+
+    res.status(200).send({ message: 'Screenshot data received and stored successfully' });
+});
 
 app.get('/screenshot', (req, res) => {
     if (screenshotData) {
         res.status(200).send({ screenshot: screenshotData });
-        console.log("sended screenshot")
+        console.log("Sent screenshot data.");
     } else {
         res.status(404).send({ error: 'No screenshot data available' });
     }
@@ -22,9 +31,8 @@ const wss = new WebSocket.Server({ server });
 
 wss.on('connection', (ws) => {
     ws.on('message', (message) => {
-
         screenshotData = message;
-        console.log("got screenshot")
+        console.log("Received screenshot via WebSocket");
 
         wss.clients.forEach((client) => {
             if (client.readyState === WebSocket.OPEN) {
